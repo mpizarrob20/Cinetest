d7a5a8b6facab6562189b32b2d4a4a07dd317bdd
May <mpizarrob20@gmail.com>
2016-08-31 15:47:26 -0300

    second commit

--- src/test/java/com/cucumber/Selenium/CineTestRunner.java
+++ src/test/java/com/cucumber/Selenium/CineTestRunner.java
@@ -1,9 +1,7 @@
 package com.cucumber.Selenium;
 
 import cucumber.api.junit.Cucumber;
-
 import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 
 
--- src/test/java/com/cucumber/Selenium/cineTest.java
+++ src/test/java/com/cucumber/Selenium/cineTest.java
@@ -3,17 +3,10 @@ package com.cucumber.Selenium;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import cucumber.api.junit.Cucumber;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
-import cucumber.api.PendingException;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
